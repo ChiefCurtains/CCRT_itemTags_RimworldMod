0fe7ea13948d0922793cdee7cb5026183650d718
Lloyd Halterman <titaniumturtle89@gmail.com>
2022-11-10 15:11:19 -0500

    Testing lists

---
+++ Core/EnumTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace CCRT_itemTags.Core
+{
+    public class TestTag
+    {
+        public void TagListInt()
+        {
+            var tagID = new List<string>
+            {
+                tag01,
+                tag02,
+                tag03,
+                tag04,
+                tag05,
+                tag06,
+                tag07,
+                tag08,
+                tag09,
+                tag10,
+                tag11,
+                tag12,
+            };
+            foreach (var tag in tagID)
+            {
+
+            }
+        }
+        private readonly string tag01;
+        private readonly string tag02;
+        private readonly string tag03;
+        private readonly string tag04;
+        private readonly string tag05;
+        private readonly string tag06;
+        private readonly string tag07;
+        private readonly string tag08;
+        private readonly string tag09;
+        private readonly string tag10;
+        private readonly string tag11;
+        private readonly string tag12;
+    }
+
+}
+
